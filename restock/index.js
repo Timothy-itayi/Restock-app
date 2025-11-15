724ef5bf0e6cfa5136bea12f52c01881e53748e7
Timothy <timmytam10@gmail.com>
2025-11-15 17:00:17 +1100

    clean tree for prebuild

--- restock/index.js
+++ restock/index.js
@@ -1,28 +1,8 @@
-// Ensure RNGH is initialized before any navigation code
-import 'react-native-gesture-handler';
+import { registerRootComponent } from 'expo';
 
-// Early boot diagnostics
-try {
-  // Better stack traces in development
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('source-map-support/register');
-} catch {}
+import App from './App';
 
-// Early boot diagnostics
-try {
-  // eslint-disable-next-line no-undef
-  if (global && global.ErrorUtils && typeof global.ErrorUtils.setGlobalHandler === 'function') {
-    // eslint-disable-next-line no-undef
-    global.ErrorUtils.setGlobalHandler((error, isFatal) => {
-      // Avoid crashing silently
-      // eslint-disable-next-line no-console
-      console.error('🚨 [entry] Global JS error', { isFatal, message: error?.message, stack: error?.stack });
-    });
-  }
-} catch {}
-
-// eslint-disable-next-line no-console
-console.log('🧭 [entry] Loading expo-router entry...');
-require('expo-router/entry');
-// eslint-disable-next-line no-console
-console.log('🧭 [entry] expo-router entry loaded');
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
